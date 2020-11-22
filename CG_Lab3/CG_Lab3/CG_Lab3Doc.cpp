c87b788302e74aea0f63478e8794ba9221af67a8
Denys Shutka <nden2105@gmail.com>
2020-11-22 13:19:27 +0200

    feat: add Lab3

---
+++ CG_Lab3/CG_Lab3/CG_Lab3Doc.cpp
@@ -0,0 +1,138 @@
+
+// CG_Lab3Doc.cpp : implementation of the CCGLab3Doc class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "CG_Lab3.h"
+#endif
+
+#include "CG_Lab3Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CCGLab3Doc
+
+IMPLEMENT_DYNCREATE(CCGLab3Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CCGLab3Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CCGLab3Doc construction/destruction
+
+CCGLab3Doc::CCGLab3Doc() noexcept
+{
+	// TODO: add one-time construction code here
+
+}
+
+CCGLab3Doc::~CCGLab3Doc()
+{
+}
+
+BOOL CCGLab3Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CCGLab3Doc serialization
+
+void CCGLab3Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Support for thumbnails
+void CCGLab3Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Modify this code to draw the document's data
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Support for Search Handlers
+void CCGLab3Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Set search contents from document's data.
+	// The content parts should be separated by ";"
+
+	// For example:  strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CCGLab3Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CCGLab3Doc diagnostics
+
+#ifdef _DEBUG
+void CCGLab3Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CCGLab3Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CCGLab3Doc commands
